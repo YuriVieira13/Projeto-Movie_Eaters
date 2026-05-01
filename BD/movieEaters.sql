d6236cd246a42337cbde1f1ccbe935a97d213747
YuriVieira13 <yuri.avieira@sptech.school>
2026-04-30 23:41:30 -0300

    subindo tela filme da semana versão diretor e versão watcher

---
+++ BD/movieEaters.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE movieEaters;
+
+USE movieEaters;
+
+CREATE TABLE usuario (
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50));
+    
+    SELECT * FROM usuario;
+
+    
+    
+    
